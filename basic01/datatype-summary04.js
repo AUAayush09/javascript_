1404e18ebcd008f547b358de428005d7e43396eb
AUAayush09 <145000609+AUAayush09@users.noreply.github.com>
2024-02-08 19:25:52 +0000

    stack and heap memory

--- basic01/datatype-summary04.js
+++ basic01/datatype-summary04.js
@@ -34,3 +34,33 @@ const myFunction = function(){
 // we can know all the datatype by using " typeof" 
 
 console.log(typeof (myFunction))
+
+
+// ***************************************************************
+
+
+//   Stack memory(primitive)  &  heap memory(non-primitive)
+
+
+// stack memory
+let userOne = "abhash@google.com"
+
+let userTwo = userOne
+userTwo = "chunnu@google.com"
+console.log(userOne);
+console.log(userTwo);
+
+// heap memory
+
+let value1 = {
+  email: "a@a.com",
+   upi: "a@ybl"
+}
+let value2 = value1
+value2.email = "aa@a.com"
+console.log(value1);
+console.log(value2);
+
+/*
+The stack is used to store static data, whereas the heap is used to store dynamic data. The stack contains references to the objects in a heap. JavaScript also uses a garbage collector to deallocate unused memory from the heap
+*/
